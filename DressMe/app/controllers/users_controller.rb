ac83068bc22329fff3485ab203905713c552144f
pamantonow <unstyleurbain@gmail.com>
2016-11-13 17:47:54 -0700

    need to finish

--- DressMe/app/controllers/users_controller.rb
+++ DressMe/app/controllers/users_controller.rb
@@ -8,17 +8,23 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			redirect_to user_path
+			session[:user_id] = @user.id
+			redirect_to user_path(@user)
 		else
-			@errors = @user.errors.full_message
+			@errors = @user.errors.full_messages
 			render :'users/new'
 		end
+	end
+
+	def show
+		@user = User.find(user_id)
+		render :'users/show'
 	end
 
 
 	private
 
 	def user_params
-		params.require(:User).permit(:first_name,:last_name,:username,:email,:password)
+		params.require(:user).permit(:first_name,:last_name,:username,:email,:password)
 	end
 end
--- DressMe/app/helpers/application_helper.rb
+++ DressMe/app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
 	def current_user
 		@current_user ||= User.find_by(id: session[:user_id])
 	end
+
+	# def user_id
+	# 	current_user.id
+	# end
 end
